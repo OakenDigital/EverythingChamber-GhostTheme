d25295bd842a76d8bd4ec3398f121c0119b3bb5c
Ron McCranie <ron@oakendigital.com>
2024-01-08 16:14:13 -0600

    homepage close to finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.hbs', './**/*.hbs'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+      'hero-copy-gradient': "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.7) 100%)",
+      'secondary-gradient': "linear-gradient(180deg,rgb(55, 102, 173) 0%,var(--color-brand-rich) 100%);",
+      },
+    },
   },
   plugins: [],
 };
